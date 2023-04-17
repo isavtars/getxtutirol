e50ac6ac1b86092b3d27e79866d5d56f9571f129
isavtars <113200845+isavtars@users.noreply.github.com>
2023-04-17 13:06:50 +0545

    GetX Bonus Features

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:get/get_navigation/get_navigation.dart';
+import 'package:get/get.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +12,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'Flutter Demo',
+      title: 'Getx tutirol',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -21,13 +22,130 @@ class MyApp extends StatelessWidget {
   }
 }
 
+// GetX Bonus Features
+
 class MyPage extends StatelessWidget {
   const MyPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder(
-      child: Text("all about getx controller"),
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(
+                child: Text(
+                  " GetX Bonus Features",
+                  style: TextStyle(fontSize: 22),
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Container(
+                padding: const EdgeInsets.all(3),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Color.fromARGB(255, 115, 9, 214),
+                              minimumSize: Size(70, 45)),
+                          onPressed: () {
+                            Get.defaultDialog(
+                              radius: 10.0,
+                              contentPadding: const EdgeInsets.all(20.0),
+                              title: 'title',
+                              middleText: 'content',
+                              textConfirm: 'Okay',
+                              confirm: OutlinedButton.icon(
+                                onPressed: () => Get.back(),
+                                icon: const Icon(
+                                  Icons.check,
+                                  color: Colors.blue,
+                                ),
+                                label: const Text(
+                                  'Okay',
+                                  style: TextStyle(color: Colors.blue),
+                                ),
+                              ),
+                              cancel: OutlinedButton.icon(
+                                onPressed: () {},
+                                icon: Icon(Icons.tap_and_play),
+                                label: Text('label'),
+                              ),
+                            );
+                          },
+                          child: const Text('dailogBox')),
+                    ),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    Expanded(
+                      child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor:
+                                  Color.fromARGB(255, 73, 156, 105),
+                              minimumSize: Size(70, 45)),
+                          onPressed: () {
+                            Get.bottomSheet(
+                              Container(
+                                height: 850,
+                                color: Colors.white,
+                                child: const Center(
+                                  child: Text(
+                                    'Count has reached ',
+                                    style: const TextStyle(
+                                        fontSize: 28.0, color: Colors.black),
+                                  ),
+                                ),
+                              ),
+                            );
+                          },
+                          child: const Text('dailogBox')),
+                    )
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Color.fromARGB(255, 26, 155, 144),
+                        minimumSize: Size(130, 45)),
+                    onPressed: () {
+                      Get.snackbar(
+                        "this is snackbar",
+                        "we cant able to snack bar in this time",
+                        snackPosition: SnackPosition.BOTTOM,
+                        titleText: const Text(
+                          "heloo",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 22,
+                              color: Colors.white),
+                        ),
+                      );
+                    },
+                    child: Expanded(
+                      child: const Text('snackbar'),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
